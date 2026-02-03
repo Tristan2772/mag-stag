616c2055e267d4cbb080e35738cc5cbc7fecdd8a
Tristan Carter <tristancarter2772@gmail.com>
2026-02-03 16:34:11 -0600

    connected products to homepage collections (#11)

--- app/lib/types.ts
+++ app/lib/types.ts
@@ -1,13 +1,35 @@
+export type ProductImages = {
+  id: string;
+  url: string;
+  width: number;
+  height: number;
+  altText: string | null;
+};
+
 export type Product = {
   id: string;
   title: string;
   available: boolean;
-  imageUrl: string | undefined;
+  images: ProductImages[];
+  price: string;
+  compareAtPrice: string;
 };
 export type ProductNode = {
   id: string;
   title: string;
   availableForSale: boolean;
+  priceRange: {
+    minVariantPrice: {
+      amount: string;
+      currencyCode: string;
+    };
+  };
+  compareAtPriceRange: {
+    minVariantPrice: {
+      amount: string;
+      currencyCode: string;
+    };
+  };
   images: {
     edges: {
       node: {
